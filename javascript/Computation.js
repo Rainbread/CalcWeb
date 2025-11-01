e8deade2cca1a0db0b2bc46d1f44521f460b3e3f
Rafael-Carrillo001 <54818054+Rafael-Carrillo001@users.noreply.github.com>
2025-11-01 11:44:49 -0400

    Basic operation is ready
    You are able to do calculation with basic operation, including with decimal.

--- javascript/Computation.js
+++ javascript/Computation.js
@@ -4,7 +4,71 @@
 
 //------Arithmetic-------
 
-function adding(num1, num2) {
+const display = document.getElementById("display");
+
+const isOp = ch => ['+', '-', '*', '/', '%'].includes(ch); //Used when user wants to continue evaluating more after their previous evaluation
+
+let justEval = false; //Used to confrimed a user has done an evaluation. 
+
+
+
+function appendToDisplay(input) {
+
+    let current = display.textContent || '';
+
+    if (current === 'Error') current = '';
+
+    if (justEval) {
+
+        if (isOp(input)) { 
+
+            display.textContent = current + input; 
+
+        } else {
+            display.textContent = (input === '.') ? '0.' : String(input); //
+        }
+
+        justEval = false;
+        return;
+    }
+
+    // Rules for an empty display
+    if (current === '') {
+        if (input === '.') { 
+            display.textContent = '0.'; //Display 0 on the left ide of "." when user press this first
+            
+            return;
+         }
+        // Don’t start with +, *, /, %, but allow "-" (negative) and "("
+        if (isOp(input) && input !== '-') return;
+    }
+    display.textContent = current + String(input);
+
+}
+
+function clearDisplay() {
+    display.textContent = ""; //When button "C" is pressed
+    justEval = false;
+
+}
+
+function calculate() {
+
+    try {
+        display.textContent = eval(display.textContent); //Used to evaluate a complete expression
+
+    }
+    catch (error) {
+        display.textContent = "Error"; //Used when an incomplete or invalid expression occur 
+
+    } finally {
+
+        justEval = true;
+    }
+}
+
+
+/*function adding(num1, num2) {
 
     const a = Number(num1);
     const b = Number(num2);
@@ -77,7 +141,7 @@ function dividing(num1, num2) {
 
     return (aIsDecimal || bIsDecimal || rawIsDecimal) ? Number(raw.toFixed(2)) : raw;
 
-}
+}*/
 
 
 
