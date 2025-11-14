0c7fac830d4ea9b8129f7cfcccbd63bf4442a55e
Rainbread <114000059+Rainbread@users.noreply.github.com>
2025-11-13 20:24:19 -0500

    Modified Computation.js
    Modified:
    - God damn that original code looked so bad, I can't even read it. So I rearrange codes, no function were modified.

--- javascript/Computation.js
+++ javascript/Computation.js
@@ -1,33 +1,17 @@
-
-
-
-
 //------Arithmetic-------
-
 const display = document.getElementById("display");
-
 const isOp = ch => ['+', '-', '*', '/', '%'].includes(ch); //Used when user wants to continue evaluating more after their previous evaluation
-
 let justEval = false; //Used to confrimed a user has done an evaluation. 
 
-
-
 function appendToDisplay(input) {
-
     let current = display.textContent || '';
-
     if (current === 'Error') current = '';
-
     if (justEval) {
-
         if (isOp(input)) { 
-
             display.textContent = current + input; 
-
         } else {
-            display.textContent = (input === '.') ? '0.' : String(input); //
+            display.textContent = (input === '.') ? '0.' : String(input);
         }
-
         justEval = false;
         return;
     }
@@ -35,34 +19,27 @@ function appendToDisplay(input) {
     // Rules for an empty display
     if (current === '') {
         if (input === '.') { 
-            display.textContent = '0.'; //Display 0 on the left ide of "." when user press this first
-            
+            display.textContent = ''; //Display 0 on the left ide of "." when user press this first 
             return;
          }
         // Don’t start with +, *, /, %, but allow "-" (negative) and "("
         if (isOp(input) && input !== '-') return;
     }
     display.textContent = current + String(input);
-
 }
 
 function clearDisplay() {
     display.textContent = ""; //When button "C" is pressed
     justEval = false;
-
 }
 
 function calculate() {
-
     try {
         display.textContent = eval(display.textContent); //Used to evaluate a complete expression
-
     }
     catch (error) {
         display.textContent = "Error"; //Used when an incomplete or invalid expression occur 
-
     } finally {
-
         justEval = true;
     }
 }
